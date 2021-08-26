ccb427e4bdf966d978388a9ba56bd026d73cfe4d
dharmasatrya <dharmasatrya10@gmail.com>
2021-08-26 13:53:13 +0700

    second deploy

--- controllers/buyerController.js
+++ controllers/buyerController.js
@@ -20,7 +20,7 @@ class buyerController {
         description: name,
         amount: price,
         shouldSendEmail: true,
-        successRedirectURL: `http://localhost:3000/buyer/buy/${id}`,
+        successRedirectURL: `https://fashion-doto.web.app/buyer/buy/${id}`,
       });
       res.status(200).json(invoice);
     } catch (error) {
